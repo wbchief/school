652075c93230ed34b98a4d3afa5551003dc93478
chief <712640388@qq.com>
2018-11-05 12:54:53 +0800

    更新管理员

--- schoolspider/models.py
+++ schoolspider/models.py
@@ -15,6 +15,8 @@ class Login(db.Model):
     username = db.Column(db.String(64), primary_key=True)
     password = db.Column(db.String(64))
     flag = db.Column(db.String(2), index=True)
+    counts = db.Column(db.Integer)
+
 
     def __repr__(self):
         return '<Login %r>' % self.username
@@ -35,4 +37,22 @@ class Login(db.Model):
             except Exception as e:
                 print(e.args)
                 db.session.rollback()
+
+    def insert_admin(self, n=100):
+        '''
+        插入管理员
+        :return:
+        '''
+        random.seed(time.time())
+        for i in range(n):
+            username = random.randrange(100000, 1000000)
+            password = random.randrange(100000, 1000000)
+            try:
+                login = Login(username=username, password=password, flag='2', counts=0)
+                db.session.add(login)
+                db.session.commit()
+            except Exception as e:
+                print(e.args)
+                db.session.rollback()
+
 
